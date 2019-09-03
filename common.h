a521ac06839660cebe482d4cf1a16792e8815f8f
Felix Abecassis <fabecassis@nvidia.com>
2019-09-03 14:26:03 -0700

    Use raw syscall for memfd_create(2)
    CentOS/RHEL 7.x installations don't provide the glibc wrapper.
    
    Closes: #3
    
    Reported-by: Kilian Cavalotti <kilian@stanford.edu>
    Signed-off-by: Felix Abecassis <fabecassis@nvidia.com>

--- common.h
+++ common.h
@@ -2,6 +2,7 @@
 #define COMMON_H_
 
 #include <unistd.h>
+#include <sys/syscall.h>
 
 #define PYXIS_RUNTIME_PATH "/run/pyxis"
 #define PYXIS_USER_RUNTIME_PATH "/run/pyxis/%d"
@@ -13,6 +14,21 @@ static inline void xclose(int fd)
 	if (fd < 0)
 		return;
 	close(fd);
+}
+
+#if !defined(MFD_CLOEXEC)
+# define MFD_CLOEXEC 0x0001U
+#endif /* !defined(MFD_CLOEXEC) */
+
+#if defined(__x86_64__)
+# if !defined(__NR_memfd_create)
+#  define __NR_memfd_create 319
+# endif /* !defined(__NR_memfd_create) */
+#endif /* defined(__x86_64__) */
+
+static inline int pyxis_memfd_create(const char *name, unsigned int flags)
+{
+	return syscall(__NR_memfd_create, name, flags);
 }
 
 #endif /* COMMON_H_ */
