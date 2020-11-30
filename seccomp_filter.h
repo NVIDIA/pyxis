aa0a7659b78eb5c0ef6800b4e2af98f633afa796
Felix Abecassis <fabecassis@nvidia.com>
2020-11-30 14:55:49 -0800

    Update seccomp filter for 32-bit binaries
    See https://github.com/NVIDIA/enroot/issues/43
    
    Signed-off-by: Felix Abecassis <fabecassis@nvidia.com>

--- seccomp_filter.h
+++ seccomp_filter.h
@@ -28,43 +28,44 @@
  * - it requires using setuid binaries newuidmap(1) and newgidmap(1)
  * - writing to a shared filesystem with a subordinate uid/gid can create surprising
  *   results for the user.
- * This code was taken from enroot: https://github.com/NVIDIA/enroot/blob/c0043113e23eccc5128b44427914d14448d61f0a/bin/enroot-unshare.c#L64-L95
+ * This code was taken from enroot: https://github.com/NVIDIA/enroot/blob/77bb892ff1738fd65c2dfc437f82fb7d57888c0b/bin/enroot-nsenter.c#L93-L129
  */
 static struct sock_filter filter[] = {
-	/* Check for the syscall architecture (x86_64/x32 and aarch64 ABIs). */
-	BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, arch)),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, AUDIT_ARCH_X86_64, 2, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, AUDIT_ARCH_AARCH64, 1, 0),
-	BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_KILL),
+	/* Check for the syscall architecture (x86_64 and aarch64 ABIs). */
+	BPF_STMT(BPF_LD|BPF_W|BPF_ABS, offsetof(struct seccomp_data, arch)),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, AUDIT_ARCH_X86_64,  2, 0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, AUDIT_ARCH_AARCH64, 1, 0),
+	/* FIXME We do not support x86, x32 and aarch32, allow all syscalls for now. */
+	BPF_STMT(BPF_RET|BPF_K, SECCOMP_RET_ALLOW),
 
 	/* Load the syscall number. */
-	BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, nr)),
+	BPF_STMT(BPF_LD|BPF_W|BPF_ABS, offsetof(struct seccomp_data, nr)),
 
-/* Return success on all the following syscalls. */
+	/* Return success on all the following syscalls. */
 #if defined(SYS_chown) && defined(SYS_lchown)
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_chown, 15, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_lchown, 14, 0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_chown,     15, 0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_lchown,    14, 0),
 #endif
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_setuid, 13, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_setgid, 12, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_setreuid, 11, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_setregid, 10, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_setresuid, 9, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_setresgid, 8, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_setgroups, 7, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_fchownat, 6, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_fchown, 5, 0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_setuid,    13, 0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_setgid,    12, 0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_setreuid,  11, 0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_setregid,  10, 0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_setresuid, 9,  0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_setresgid, 8,  0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_setgroups, 7,  0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_fchownat,  6,  0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_fchown,    5,  0),
 
 	/* For setfsuid/setfsgid we only return success if the uid/gid argument is not -1. */
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_setfsuid, 1, 0),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_setfsgid, 0, 2),
-	BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, args[0])),
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, (uint32_t)-1, 0, 1),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_setfsuid, 1, 0),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, SYS_setfsgid, 0, 2),
+	BPF_STMT(BPF_LD|BPF_W|BPF_ABS, offsetof(struct seccomp_data, args[0])),
+	BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, (uint32_t)-1, 0, 1),
 
 	/* Execute the syscall as usual. */
-	BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+	BPF_STMT(BPF_RET|BPF_K, SECCOMP_RET_ALLOW),
 	/* Return success. */
-	BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ERRNO | (SECCOMP_RET_DATA & 0x0)),
+	BPF_STMT(BPF_RET|BPF_K, SECCOMP_RET_ERRNO|(SECCOMP_RET_DATA & 0x0)),
 };
 
 static inline int seccomp_set_filter(void)
