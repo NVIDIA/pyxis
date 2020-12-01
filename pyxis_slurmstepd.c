8ffbea885d28fe4e80bce0653dd273647d4648b7
Felix Abecassis <fabecassis@nvidia.com>
2020-12-01 09:09:39 -0800

    Fix log level when reporting an error with --container-name
    Signed-off-by: Felix Abecassis <fabecassis@nvidia.com>

--- pyxis_slurmstepd.c
+++ pyxis_slurmstepd.c
@@ -877,8 +877,8 @@ int slurm_spank_user_init(spank_t sp, int ac, char **av)
 			slurm_info("pyxis: reusing existing container filesystem");
 			context.container.reuse_rootfs = true;
 		} else if (context.args->image == NULL) {
-			slurm_info("pyxis: error: a container with name \"%s\" does not exist, and --container-image is not set",
-				   context.args->container_name);
+			slurm_error("pyxis: error: a container with name \"%s\" does not exist, and --container-image is not set",
+				    context.args->container_name);
 			goto fail;
 		}
 		context.container.name = container_name;
